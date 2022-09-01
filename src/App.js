483230a1af710f7b9bf982604fd54aebace58294
diamond-nicholas <diamondnicholas154@gmail.com>
2022-09-01 23:41:02 +0100

    update structure and page navigation ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Nav from './Components/Navigation/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='homepage'>
+      <section className='main'>
+        <article className='nav-wrapper'>
+          <Nav />
+        </article>
+      </section>
+      <section className='filter'></section>
     </div>
   );
 }
---
+++ src/Components/Navigation/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+};
+
+export default Nav;
