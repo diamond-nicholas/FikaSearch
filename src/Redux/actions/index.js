0a995db49f9dc59adc26bcd48275c85d85b57625
diamond-nicholas <diamondnicholas154@gmail.com>
2022-09-03 10:53:40 +0100

    add reducer for genres

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -4,3 +4,8 @@ export const data = (sdks) => ({
   type: actions.INSTALLED_SDK,
   payload: sdks,
 });
+
+export const genreList = (genre) => ({
+  type: actions.GENRES,
+  payload: genre,
+});
--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -1,9 +1,11 @@
 import installedReducer from './installed';
+import genreReducer from './genre';
 
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
   install: installedReducer,
+  genre: genreReducer,
 });
 
 export default allReducers;
